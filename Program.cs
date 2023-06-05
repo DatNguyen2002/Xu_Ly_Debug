f8f9779d653aabf3e8f326d344f5cc3594accde3
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-06-05 20:50:55 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace updateCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.AddRange(new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+
+                if (characters.Count >= 7)
+                {
+                    char value = characters[6];
+                    Console.WriteLine(value);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid index");
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
